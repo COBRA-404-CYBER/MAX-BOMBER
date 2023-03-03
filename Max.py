ab89d1645d67f702f96e5c43c822915701b0de7b
Farhan Khan <121663493+COBRA-404-CYBER@users.noreply.github.com>
2023-03-03 15:30:13 +0600

    Add files via upload

---
+++ Max.py
@@ -0,0 +1,10 @@
+import os,time
+os.system("clear")
+print("\n\033[38;5;195m[\033[38;5;46m+\033[38;5;195m]\033[38;5;46m CHECKING UPDATE 💚")
+time.sleep(2)
+os.system("git pull")
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/groups/1354738058401296/?ref=share&mibextid=NSMWB")
+
+import ax
